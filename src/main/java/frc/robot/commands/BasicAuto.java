302699bc5b0a58c445826d01b94ede491899e7dd
Scgjle <ianplymale@gmail.com>
2024-11-25 17:40:06 -0800

    New auto Fixes

--- src/main/java/frc/robot/commands/BasicAuto.java
+++ src/main/java/frc/robot/commands/BasicAuto.java
@@ -8,15 +8,13 @@ import frc.robot.subsystems.Shooter;
 
 public class BasicAuto extends SequentialCommandGroup{
     public BasicAuto(Drivetrain drivetrain, Arm arm,Shooter shooter){
-        addCommands {
-            new AutoDrive(drivetrain, 0.25, 0.25).withTimeout(1),
+            // turn right 
+            new AutoDrive(drivetrain, -0.25, 0.25).withTimeout(1),
 
             new WaitCommand(3),
     
             new LiftArm(arm, 0.4).withTimeout(0.5),
     
             new Shoot(shooter, 5).withTimeout(2)
-        };
-
     }
 }
