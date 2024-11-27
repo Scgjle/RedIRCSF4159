14117713991fc531e4427c9f5750340fefc47de3
Scgjle <ianplymale@gmail.com>
2024-11-26 20:46:22 -0800

    New auto changes

--- src/main/java/frc/robot/commands/TestAuto.java
+++ src/main/java/frc/robot/commands/TestAuto.java
@@ -8,11 +8,14 @@ import frc.robot.subsystems.Shooter;
 
 public class BasicAuto extends SequentialCommandGroup{
              
-    public BasicAuto(Drivetrain drivetrain, Arm arm,Shooter shooter){
+    public TestAuto(Drivetrain drivetrain, Arm arm,Shooter shooter){
 
         addCommands(
             //shoots the ball
             new Shoot(shooter, 1).withTimeout(1),
+
+            //lifts arm or pivits shooter
+            new LiftArm(arm, 0.2).withTimeout(1),
 
             //moves the robot left
             new AutoDrive(drivetrain, -0.2, 0.2).withTimeout(1),
